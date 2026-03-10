49f1d61bcbea006a21d76f31186c0362b504cf40
Evelina <evelinazolota55@gmail.com>
2026-03-10 23:37:23 +0300

    Спринт 16: обнова 1

--- src/main/java/ru/practicum/shareit/common/ForbiddenException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.common;
-
-public class ForbiddenException extends RuntimeException {
-    public ForbiddenException(String message) {
-        super(message);
-    }
-}
